9e94d847db8cf7c3627073cacea09d8f3aacfb54
Roman <romchikus1987@gmail.com>
2025-02-12 06:38:05 +0200

    updated config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,26 @@
 require("@nomicfoundation/hardhat-toolbox")
+require("hardhat-contract-sizer");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.22",
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 1,
-    },
-    viaIR: true,   
-    metadata: {
-      bytecodeHash: "none",
-    },    
-    debug: {
-      revertStrings: "strip"
+  solidity: {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1,
+      },
+      viaIR: true,
+      metadata: {
+        bytecodeHash: "none",
+      },
+      debug: {
+        revertStrings: "strip"
+      }
     }
   },
+  contractSizer: {
+    runOnCompile: true,  // Runs automatically when compiling
+    only: ["MyGovernor"], // Specify contracts to measure (optional)
+  }
 };
+
